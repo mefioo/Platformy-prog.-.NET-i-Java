caf0cab0e06d0f32293cca0a69d10a1262e722a7
mefioo <38735691+mefioo@users.noreply.github.com>
2019-03-13 00:13:12 +0100

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Person> people = new ObservableCollection<Person>
+        {
+           // new Person { Text1 = "P1", Text2 = "P11", Text3 = "im5.jpg" },
+           // new Person { Text1 = "P2", Text2 = "P22", Text3 = "im2.jpg" },
+        };
+
+        public ObservableCollection<Person> Items
+        {
+            get => people;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+       
+
+        string path;
+        private void Loadbtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+            path = op.FileName;
+
+        }
+        private void AddNewPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            people.Add(new Person { Text2 = TextBox2.Text, Text1 = TextBox1.Text, Text3 = path });
+        }
+    }
+}
