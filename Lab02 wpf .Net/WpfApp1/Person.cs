4c6b8f6001e1fe2632ae41ed68113d6e055e051f
mefioo <38735691+mefioo@users.noreply.github.com>
2019-03-20 22:09:25 +0100

    Add files via upload

---
+++ Lab02 wpf .Net/WpfApp1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Lab01
+{
+    public class Person
+    {
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+        public string Text3 { get; set; }
+    }
+}
